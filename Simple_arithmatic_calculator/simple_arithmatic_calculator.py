05f5feb196a697ebd27e40b5eb9f6d26105b2cd2
rohan280804 <rohan.bharadwaj28@gmail.com>
2025-09-02 14:02:37 +0530

    first commit

---
+++ Simple_arithmatic_calculator/simple_arithmatic_calculator.py
@@ -0,0 +1,13 @@
+operator = input("Enter an operator(+,-,/,*): ")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+if operator == "+":
+    print(num1+num2)
+elif operator == "-":
+    print(num1-num2)
+elif operator == "*":
+    print(num1*num2)
+else:
+    print(num1/num2)
+
+    
