6766cb17c8ef54b84061a9e90b3ccf56106b60b7
rohan280804 <rohan.bharadwaj28@gmail.com>
2025-09-03 12:28:06 +0530

    Added new files

---
+++ Encryption_program/encryption_program.py
@@ -0,0 +1,35 @@
+import random
+import string
+
+chars = string.punctuation + string.digits + string.ascii_letters + " "
+
+chars = list(chars)
+key = chars.copy()
+
+random.shuffle(key)
+
+#ENCRYPTION
+
+plain_text = input("Enter a message to encrypt: ")
+cipher_text = ""
+
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+
+print(f"Original Message: {plain_text}")
+print(f"Encrypted Message: {cipher_text}")
+
+print()
+#DECRYPTION
+
+cipher_text = input("Enter a message to decrypt: ")
+plain_text = ""
+
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += chars[index]
+
+print(f"Encrypter Message: {cipher_text}")
+print(f"Original Message: {plain_text}") 
+print()
